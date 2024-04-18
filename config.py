ec5e52c53dee5bb1f06c4e98008d191ce7f466c6
MrR3353 <mishinstepan2000@gmail.com>
2024-04-18 11:23:51 +0300

    Добавил документацию и батник для запуска через терминал

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 from pathlib import Path
 
 BASE_PATH = Path(os.getcwd())
-BASE_PATH = Path(os.path.join(BASE_PATH, 'folder'))
+# print('BASE_PATH', BASE_PATH)
+# BASE_PATH = Path(os.path.join(BASE_PATH, 'folder'))
 VCS_FOLDER = Path(os.path.join(BASE_PATH, '.vcs'))
 DATA_FOLDER = Path(os.path.join(VCS_FOLDER, 'data'))
 HEAD_PATH = Path(os.path.join(VCS_FOLDER, 'HEAD'))
---
+++ vcs.bat
@@ -0,0 +1,2 @@
+@echo off
+python D:\PycharmProjects\diplom\vcs.py %*
